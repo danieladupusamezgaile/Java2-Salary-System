a5569cf60b886fd5aed7178178097864aaecca23
Daniela <daniela.dupusamezgaile@gmail.com>
2024-04-21 22:04:42 +0300

    created PartTimeEmployee class

---
+++ PartTimeEmployee.java
@@ -0,0 +1,26 @@
+public class PartTimeEmployee extends Employee {
+    private String position = "Part Time Employee";
+    private double hourRate = 8.50;
+    private int hours;
+    private double salary;
+
+    PartTimeEmployee() {
+        setPosition(position);
+    }
+
+    void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    void income() {
+        this.salary = this.hourRate * this.hours;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()
+                + "Position: " + this.position + "\n"
+                + "Number of hours of work: " + this.hours + "\n"
+                + "Salary: " + this.salary + "Eur";
+    }
+}
